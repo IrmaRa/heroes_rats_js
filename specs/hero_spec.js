7d71f40baa04af57bb32d3e06215a49dfca26d85
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-06-16 11:46:19 +0100

    Add hero constructor

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -12,12 +12,15 @@ describe("Hero", function() {
 
   it('should have a name', function() {
     assert.strictEqual("Billy", hero.name);
-  })
+  });
+
+  it('should have a health', function() {
+    assert.strictEqual(0, hero.health);
+  });
 });
 
 
 
-// - A Hero has health
 // - A Hero has a favourite food
 // - A Hero can talk saying their name
 // - A Hero has a collection of tasks to complete
